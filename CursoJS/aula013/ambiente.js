eb89b93bd806d0d76d5b59d5be603e64219cae6d
Marcus Del Pino <96274051+MarcusDelPino@users.noreply.github.com>
2021-12-22 20:54:23 -0300

    estudando for

--- CursoJS/aula013/ambiente.js
+++ CursoJS/aula013/ambiente.js
@@ -1,8 +1,17 @@
 //console.log(`tudo bem?`)
 
-var c = 1
-while (c <= 6) {
-    console.log(`${c} <`)
-    c++
-}
 
+
+/*var c = 1
+var d = 10
+do {
+    console.log(`${c} e ${d}`)
+    c++
+    d--
+}while (c <= 6 && d >= 5) 
+
+var c = 1
+do {
+    console.log(`${c} `)
+    c++
+}while (c <= 6) */
---
+++ CursoJS/aula014/ambiente.js
@@ -0,0 +1,5 @@
+console.log('Vai Começar a putaria')
+for (var c = 1;c <= 5;c++){
+    console.log(`${c} `)
+}
+console.log('Fim')
