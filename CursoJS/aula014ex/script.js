af71a287bbd966647d288b301f5d65b98d6a7008
mkthaking <marcusdelpino14@gmail.com>
2021-12-23 12:49:31 -0300

    Entendi meu erro, vou refazer

--- CursoJS/aula014ex/script.js
+++ CursoJS/aula014ex/script.js
@@ -1,9 +1,16 @@
 var inicio = window.document.querySelector('input#txtnumber')
+var termino = window.document.querySelector('input#txtnumber1')
+var passo = window.document.querySelector('input#txtnumber2')
 var res = window.document.querySelector('div#res')
 var btn = window.document.querySelector('input#btn')
 btn.addEventListener('click', clicar)
 
+/*
 function clicar(params) {
     var n1 = Number(inicio.value)
     res.innerHTML = ` ${n1} `
+}*/
+
+function name(params) {
+    
 }
