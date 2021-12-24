8855889726a5439c14a1d5d642c3364fd88b3cde
Marcus Del Pino <96274051+MarcusDelPino@users.noreply.github.com>
2021-12-24 17:09:21 -0300

    tentando

---
+++ CursoJS/aula017ex/script.js
@@ -0,0 +1,12 @@
+var num = window.document.querySelector('input#txtnumber')
+var lista = window.document.querySelector('select#list')
+var res = window.document.querySelector('div#res')
+let valores = []
+
+function adicionar() {
+    if (isnumero(num.bvaleu) && !inlista(num.value)) {
+        
+    }else{
+        
+    }
+}
