68e2c8b16155cda635280db55b1cff1ef01d9b42
Marcus Del Pino <96274051+MarcusDelPino@users.noreply.github.com>
2021-12-23 21:31:52 -0300

    estudo function + array

---
+++ CursoJS/aula015/ambiente.js
@@ -0,0 +1,16 @@
+let num = [5, 8, 2, 9]
+
+//console.log(`Nosso vetor é o ${num} `)
+
+num[4] = 1
+
+//console.log(num[4])
+
+num.push(86)
+num.push(74)
+num.push(333)
+console.log(num.length)
+console.log(`${num.sort()} `)
+//num.sort()
+let pos = num.indexOf(8)
+console.log(`Ovalor 8 está em ${pos} `)
