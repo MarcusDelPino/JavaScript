3a5df58862a4dbdb9788032fd552046d33657f12
Marcus Del Pino <96274051+MarcusDelPino@users.noreply.github.com>
2021-12-25 01:49:57 -0300

    tentando mais

--- CursoJS/aula017ex/script.js
+++ CursoJS/aula017ex/script.js
@@ -5,20 +5,39 @@ let valores = []
 
 
 function isnumero(n) {
-    if (num > 0 ) {
-        
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    }else {
+        return false
     }
 }
 
 function inlista(n, l) {
-    
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    }else {
+        return false
+    }
 }
 
 
 function adicionar() {
-    if (isnumero(num.bvaleu) && !inlista(num.value, valores)) {
-        
+    if (isnumero(num.value) && !inlista(num.value, valores)) {
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `O valor escolhido foi ${num.value} `
+        lista.appendChild(item)
+        res.innerHTML = ''
     }else{
-        
+        window.alert('os números tem que ser entre 0 e 100, e não pode repitir')
     }
+    num.value = ''
+    num.focus()
+}
+
+function concluir(params) {
+    res.innerHTML = `<p>Você selecionou ${valores.length}</p>`
+    var min = Math.min.apply(valores)
+    res.innerHTML += `O valor mínimo foi ${Number(min.value)} `
+    
 }
