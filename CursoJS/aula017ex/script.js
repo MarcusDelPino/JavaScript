27426af8d3624a91df579e3241397377d4119bbe
Marcus Del Pino <96274051+MarcusDelPino@users.noreply.github.com>
2021-12-25 00:17:18 -0300

    começando pos natal

--- CursoJS/aula017ex/script.js
+++ CursoJS/aula017ex/script.js
@@ -3,8 +3,20 @@ var lista = window.document.querySelector('select#list')
 var res = window.document.querySelector('div#res')
 let valores = []
 
+
+function isnumero(n) {
+    if (num > 0 ) {
+        
+    }
+}
+
+function inlista(n, l) {
+    
+}
+
+
 function adicionar() {
-    if (isnumero(num.bvaleu) && !inlista(num.value)) {
+    if (isnumero(num.bvaleu) && !inlista(num.value, valores)) {
         
     }else{
         
