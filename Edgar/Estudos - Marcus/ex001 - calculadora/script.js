7e9ee69f28091d5c834011db68c82dbb5e119af7
Marcus Del Pino <96274051+MarcusDelPino@users.noreply.github.com>
2022-02-06 00:52:21 -0300

    Aulas ed fazendo calc

--- Edgar/Estudos - Marcus/ex001 - calculadora/script.js
+++ Edgar/Estudos - Marcus/ex001 - calculadora/script.js
@@ -1,9 +1,45 @@
+const memoriaNum = []
+const visor = document.querySelector('input#visor')
 
-
-function inserirNum(num) { 
-    elementoVisor.inserirNum = num
+function inserirNum(num) {
+    
+   return visor.value += num
 }
 
-function elementoVisor() {
-    document.getElementById('visor')
+function inserirMetodo(acao){
+    memoriaNum.unshift(visor.value)
+    visor.value = ''
+    
+    if(acao == '+'){
+        let num1 = memoriaNum[1]
+        let num0 = memoriaNum[0]
+        let resultado = parseFloat(num1) + parseFloat(num0)
+        visor.value = resultado   
+       
+        
+    }else if (acao == '-'){
+        let num1 = memoriaNum[1]
+        let num0 = memoriaNum[0]
+        let resultado = parseFloat(num1) - parseFloat(num0)
+        visor.value = resultado   
+       
+        
+    }else if (acao == '*'){
+        let num1 = memoriaNum[1]
+        let num0 = memoriaNum[0]
+        let resultado = parseFloat(num1) * parseFloat(num0)
+        visor.value = resultado   
+       
+        
+    }else if(acao == '/'){
+        let num1 = memoriaNum[1]
+        let num0 = memoriaNum[0]
+        let resultado = parseFloat(num1) / parseFloat(num0)
+        visor.value = resultado   
+               
+    }else{
+
+    }
+    //memoriaNum.unshift(visor.value)
+    console.log(memoriaNum)
 }
